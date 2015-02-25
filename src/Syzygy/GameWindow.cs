1aced891e20b80bf53c08a31bbc9317fecb65284
amulware <amul@amulware.net>
2015-02-25 17:07:03 +0100

    added basic gamewindow

---
+++ src/Syzygy/GameWindow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using amulware.Graphics;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace Syzygy
+{
+    sealed class GameWindow : amulware.Graphics.Program
+    {
+        public GameWindow()
+            :base(1290, 720, GraphicsMode.Default, "Syzygy",
+            GameWindowFlags.Default, DisplayDevice.Default, 3, 2, GraphicsContextFlags.Default)
+        {
+            
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, this.Width, this.Height);
+        }
+
+        protected override void OnUpdate(UpdateEventArgs e)
+        {
+
+        }
+
+        protected override void OnRender(UpdateEventArgs e)
+        {
+            GL.ClearColor(0, 0, 0, 0);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+
+            this.SwapBuffers();
+        }
+    }
+}
--- src/Syzygy/Program.cs
+++ src/Syzygy/Program.cs
@@ -1,9 +1,11 @@
+
 namespace Syzygy
 {
     class Program
     {
         static void Main(string[] args)
         {
+            new GameWindow().Run(60);
         }
     }
 }
