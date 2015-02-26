d0aeea99bc0741cc5dcf2cf7cccb0fe89d85bb87
amulware <amul@amulware.net>
2015-02-26 17:08:14 +0100

    added connection form

--- src/Syzygy/ConnectionForm.Designer.cs
+++ src/Syzygy/ConnectionForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Syzygy
+namespace Syzygy
 {
     partial class ConnectionForm
     {
@@ -28,11 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.hostButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ipTextBox
+            // 
+            this.ipTextBox.Location = new System.Drawing.Point(12, 35);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(169, 20);
+            this.ipTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server IP address";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(90, 70);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Join";
+            this.connectButton.UseVisualStyleBackColor = true;
+            // 
+            // hostButton
+            // 
+            this.hostButton.Location = new System.Drawing.Point(171, 70);
+            this.hostButton.Name = "hostButton";
+            this.hostButton.Size = new System.Drawing.Size(99, 23);
+            this.hostButton.TabIndex = 3;
+            this.hostButton.Text = "Create Server";
+            this.hostButton.UseVisualStyleBackColor = true;
+            // 
+            // ConnectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "ConnectionForm";
+            this.ClientSize = new System.Drawing.Size(282, 105);
+            this.ControlBox = false;
+            this.Controls.Add(this.hostButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ipTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ConnectionForm";
+            this.Text = "Setup Connection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox ipTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button hostButton;
     }
 }
