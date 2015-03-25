82ae9a5a696632c5a7a56979d44358436cf393c8
amulware <amul@amulware.net>
2015-03-25 18:19:48 +0100

    added aim direction for shooting debug particles

--- src/Syzygy/Game/EconomyController.cs
+++ src/Syzygy/Game/EconomyController.cs
@@ -17,22 +17,34 @@ namespace Syzygy.Game
 
         public Id<Player> PlayerId { get { return this.player.ID; } }
 
+        private IAction rotateClockwiseAction;
+        private IAction rotateCounterClockwiseAction;
+        private IAction shootAction;
+
+        private Direction2 aimDirection;
+
         public EconomyController(GameState game, Id<Player> player)
             : base(game)
         {
             this.player = game.Players[player];
             this.body = game.Economies.First(e => e.Player == this.player).Body;
+
+            this.rotateClockwiseAction = KeyboardAction.FromKey(Key.Right);
+            this.rotateCounterClockwiseAction = KeyboardAction.FromKey(Key.Left);
+            this.shootAction = KeyboardAction.FromKey(Key.Space);
         }
 
         public override void Update(TimeSpan t)
         {
+            var rotation = this.rotateCounterClockwiseAction.AnalogAmount - this.rotateClockwiseAction.AnalogAmount;
 
-            if (InputManager.IsKeyHit(Key.Space))
+            this.aimDirection += 2f.Radians() * rotation * (float)t.NumericValue;
+    
+            if (this.shootAction.Hit)
             {
-                var request = ShootDebugParticleFromPlanet.Request(this.game, this, this.body, Direction2.Zero);
+                var request = ShootDebugParticleFromPlanet.Request(this.game, this, this.body, this.aimDirection);
                 this.game.RequestHandler.TryDo(request);
             }
-
         }
 
         public override void Draw(GeometryManager geos)
@@ -44,8 +56,16 @@ namespace Syzygy.Game
             geo.LineWidth = 0.1f;
 
             var shape = this.body.Shape;
+            var center = shape.Center.Vector;
 
-            geo.DrawCircle(shape.Center.Vector, shape.Radius.NumericValue + 0.2f, false);
+            var r = shape.Radius.NumericValue + 0.2f;
+
+            geo.DrawCircle(center, r, false);
+
+            var dVector = this.aimDirection.Vector;
+
+            geo.LineWidth = 0.05f;
+            geo.DrawLine(center + dVector * r, center + dVector * (r + 0.25f));
 
         }
 
